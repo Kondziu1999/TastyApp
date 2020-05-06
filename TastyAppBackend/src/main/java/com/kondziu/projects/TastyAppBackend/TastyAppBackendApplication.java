9234e8db6009c9ed3124d6644048d1e0678b1800
Konrad_marny_specjalista <konrad.podgorski1999@gmail.com>
2020-05-06 16:42:43 +0200

    startup project for backend side

---
+++ TastyAppBackend/src/main/java/com/kondziu/projects/TastyAppBackend/TastyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kondziu.projects.TastyAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyAppBackendApplication.class, args);
+	}
+
+}
---
+++ TastyAppBackend/src/test/java/com/kondziu/projects/TastyAppBackend/TastyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kondziu.projects.TastyAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
