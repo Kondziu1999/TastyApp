412f48a02e8cfe620c05afb67fe346448292da7b
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2020-05-19 13:16:20 +0200

    create controller advise for exception handling from uplad files controller and add exception handling to uploadImage controller

--- TastyAppBackend/src/main/java/com/kondziu/projects/TastyAppBackend/controllers/FileUploadController.java
+++ TastyAppBackend/src/main/java/com/kondziu/projects/TastyAppBackend/controllers/FileUploadController.java
@@ -2,13 +2,18 @@ package com.kondziu.projects.TastyAppBackend.controllers;
 
 
 import com.kondziu.projects.TastyAppBackend.exceptions.BadRequestException;
+import com.kondziu.projects.TastyAppBackend.exceptions.RecipeNotFoundException;
+import com.kondziu.projects.TastyAppBackend.exceptions.UserNotFoundException;
 import com.kondziu.projects.TastyAppBackend.models.ImageModel;
+import com.kondziu.projects.TastyAppBackend.repos.RecipeRepository;
+import com.kondziu.projects.TastyAppBackend.repos.UserRepository;
 import com.kondziu.projects.TastyAppBackend.services.UploadImageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,17 +22,26 @@ import java.util.UUID;
 public class FileUploadController {
 
     private final UploadImageService uploadImageService;
+    private final UserRepository userRepository;
+    private final RecipeRepository recipeRepository;
 
-    public FileUploadController(UploadImageService uploadImageService){
+    public FileUploadController(UploadImageService uploadImageService, UserRepository userRepository, RecipeRepository recipeRepository){
         this.uploadImageService=uploadImageService;
+        this.userRepository = userRepository;
+        this.recipeRepository = recipeRepository;
     }
 
     @PostMapping("/images/{optionalUserId}/{optionalRecipeId}")
-    public ResponseEntity<?> uploadImage(@PathVariable Optional<Integer> optionalUserId, @PathVariable Optional<Integer> optionalRecipeId,
+    public ResponseEntity<?> uploadImage(@PathVariable Optional<Integer> optionalUserId,
+                                         @PathVariable Optional<Integer> optionalRecipeId,
                                          @RequestParam("imageFile")MultipartFile file){
 
+        //check if request is valid
         Integer userId=optionalUserId.orElseThrow( () ->new BadRequestException("user id not specified") );
         Integer recipeId=optionalRecipeId.orElseThrow( () -> new BadRequestException("recipe id not specified") );
+        if(! userRepository.existsById(userId.longValue()) ) throw new UserNotFoundException("user with given id not found: "+userId);
+        if(! recipeRepository.existsById(recipeId) ) throw new RecipeNotFoundException("file with given id not found: "+ recipeId);
+
         if(file.isEmpty()){
             throw new BadRequestException("the attached file is empty");
         }
---
+++ TastyAppBackend/src/main/java/com/kondziu/projects/TastyAppBackend/exceptions/FileUploadControllerHandler.java
@@ -0,0 +1,49 @@
+package com.kondziu.projects.TastyAppBackend.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.util.WebUtils;
+
+import java.util.Collections;
+import java.util.List;
+
+@ControllerAdvice
+public class FileUploadControllerHandler {
+
+    @ExceptionHandler({RecipeNotFoundException.class})
+    public final ResponseEntity<ApiError> handleException(Exception ex, WebRequest request) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if (ex instanceof RecipeNotFoundException) {
+            HttpStatus status = HttpStatus.NOT_FOUND;
+            RecipeNotFoundException exception = (RecipeNotFoundException) ex;
+
+            return handleRecipeNotFoundException(exception,httpHeaders,status,request);
+        }
+        else {
+            HttpStatus status=HttpStatus.INTERNAL_SERVER_ERROR;
+            return handleExceptionInternal(ex, null,httpHeaders,status,request);
+        }
+    }
+
+    protected ResponseEntity<ApiError> handleRecipeNotFoundException(
+            RecipeNotFoundException ex,HttpHeaders headers, HttpStatus status, WebRequest request){
+        List<String> errors=Collections.singletonList(ex.getMessage());
+        return handleExceptionInternal(ex,new ApiError(errors),headers,status,request);
+    }
+
+    //single method to customize response type of all exceptions
+    protected ResponseEntity<ApiError> handleExceptionInternal(
+            Exception ex, ApiError body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
+        }
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+}
