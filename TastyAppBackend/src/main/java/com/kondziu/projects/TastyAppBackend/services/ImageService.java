7e30b61c2e823aca6737c7bd683e36b3cfadf8bc
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2020-05-23 13:01:48 +0200

    add image compression, max size 300KB, max file input 15MB, max payload 15MB

--- TastyAppBackend/src/main/java/com/kondziu/projects/TastyAppBackend/services/ImageService.java
+++ TastyAppBackend/src/main/java/com/kondziu/projects/TastyAppBackend/services/ImageService.java
@@ -1,4 +1,5 @@
 package com.kondziu.projects.TastyAppBackend.services;
+import java.io.File;
 import java.nio.file.Files;
 
 
@@ -7,6 +8,7 @@ import com.kondziu.projects.TastyAppBackend.dto.ImageDto;
 import com.kondziu.projects.TastyAppBackend.models.ImageModel;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.awt.image.BufferedImage;
@@ -25,7 +27,11 @@ import java.util.stream.Stream;
 @Service
 @Slf4j
 public class ImageService {
+    private FileManager fileManager;
 
+    public ImageService(FileManager fileManager){
+        this.fileManager=fileManager;
+    }
     private  final String UPLOADED_FOLDER="C:\\Users\\priva\\Desktop\\TastyApp\\uploads";
 
     public boolean uploadImage(ImageModel imageModel, Integer userId, Integer recipeId){
@@ -41,10 +47,10 @@ public class ImageService {
             path= path.resolveSibling(path.getFileName() + extension);
 
             System.out.println(path.toAbsolutePath());
-            FileManager.createDirIfNotExists(dirPath);
+            fileManager.createDirIfNotExists(dirPath);
 
             Files.write(path,bytes);
-
+            fileManager.compressImage(userId,recipeId,path.getFileName().toString(),UPLOADED_FOLDER,bytes.length);
             return true;
 
         }
@@ -54,7 +60,7 @@ public class ImageService {
         }
     }
     public ImageDto getImage(Integer userId, Integer recipeId,String filename){
-        String filePath = FileManager.constructPathToImage(userId,recipeId,filename,UPLOADED_FOLDER);
+        String filePath = fileManager.constructPathToImage(userId,recipeId,filename,UPLOADED_FOLDER);
         Path path = Path.of(filePath);
 
         ImageDto imageDto = new ImageDto();
